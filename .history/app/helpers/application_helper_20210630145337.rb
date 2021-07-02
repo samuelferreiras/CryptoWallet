bf0ff0c3840e7ac9adf55d233e7e9193b7b0a1e7
Samuel Ferreira <samuelmarketingd@gmail.com>
2021-07-02 14:42:10 -0300

    modificação

---
+++ .history/app/helpers/application_helper_20210630145337.rb
@@ -0,0 +1,18 @@
+module ApplicationHelper
+    def locale(locale)
+        if I18n.locale == :<% en
+    
+    def data_br(data_us)
+        data_us.strftime("%d/%m/%Y")
+    end
+    
+    def ambiente_rails
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Rails.env.production?
+            "Produção"
+        else
+            "Teste"
+        end    
+    end
+end
