1584c4ea4f4edc14bbca5fde14ce6eefc769a3fb
Samuel Ferreira <samuelmarketingd@gmail.com>
2021-07-21 13:48:38 -0300

    Mixing Ruby code with CSS to choose the background color

---
+++ .history/config/initializers/assets_20210720150508.rb
@@ -0,0 +1,25 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += %w( welcome.css )
+
+Rails.application.config.assets.precompile += %w( welcome.js )
+
+Rails.application.config.assets.precompile += %w( coins.css )
+
+Rails.application.config.assets.precompile += %w( coins.js )
+
+Rails.application.config.assets.precompile += %w( mining_types.css )
+
+Rails.application.config.assets.precompile += %w( mining_types.js )
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
